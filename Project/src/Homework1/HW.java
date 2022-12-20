e6088b50a9078923a44257c53d6a1e3bd5882842
NikaIlina <120678649+NikaIlina@users.noreply.github.com>
2022-12-20 20:26:46 +0200

    HW1

---
+++ Project/src/Homework1/HW.java
@@ -0,0 +1,54 @@
+package Homework1;
+
+public class HW {
+    public static void main(String[] args){
+        //task 1
+        byte a = -10;
+        byte b = 15;
+        short c = -3200;
+        short d = 32000;
+        int e = -2047483648;
+        int f = 2047483648;
+        long g = -12345678909876543L;
+        long h = 12345678909876543L;
+
+        System.out.println("byte a = "+a);
+        System.out.println("byte b = "+b);
+        System.out.println("short c = "+c);
+        System.out.println("short d = "+d);
+        System.out.println("int e = "+e);
+        System.out.println("int f = "+f);
+        System.out.println("long g = "+g);
+        System.out.println("long h = "+h);
+        //task 2
+        float i = 1.5F;
+        float j = -1.5F;
+        double k = 5.5;
+        double l = -5.5;
+        boolean m = true;
+        boolean n = false;
+
+        System.out.println("float i = "+i);
+        System.out.println("float j = "+j);
+        System.out.println("double k = "+k);
+        System.out.println("double l = "+l);
+        System.out.println("boolean m = "+m);
+        System.out.println("boolean n = "+n);
+        //task 3
+        char letter = 'n';
+        char num = 1;
+        char code = '\u2764';
+        char quotes = '"';
+        char cd = 0x0950;
+
+        System.out.println("Chart1 = "+letter);
+        System.out.println("Chart2 = "+num);
+        System.out.println("Chart3 = "+code);
+        System.out.println("Chart4 = "+quotes);
+        System.out.println("Chart5 = "+cd);
+
+
+
+
+    }
+}
