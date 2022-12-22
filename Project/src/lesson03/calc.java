638ceca67a4d1a7caaba5c8443a721ac223bcc59
NikaIlina <120678649+NikaIlina@users.noreply.github.com>
2022-12-22 21:51:53 +0200

    arithtop

---
+++ Project/src/lesson03/calc.java
@@ -0,0 +1,27 @@
+package lesson03;
+
+import java.util.Scanner;
+
+public class calc {
+    public static void main(String[] args) {
+
+        double firstnum;
+        double secstnum;
+        double res;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Введите первое число: ");
+        firstnum = input.nextDouble();
+        System.out.println("Введите первое число: ");
+        secstnum = input.nextDouble();
+
+    System.out.println("firstnum = " + firstnum);
+        System.out.println("secstnum = " + secstnum);
+        System.out.println(firstnum + "+" + secstnum + "=" + (firstnum + secstnum));
+        System.out.println(firstnum + "-" + secstnum + "=" + (firstnum - secstnum));
+        System.out.println(firstnum + "x" + secstnum + "=" + (firstnum * secstnum));
+        System.out.println(firstnum + "%" + secstnum + "=" + (firstnum % secstnum));
+        System.out.println(firstnum + "/" + secstnum + "=" + (firstnum / secstnum));
+    }
+}
