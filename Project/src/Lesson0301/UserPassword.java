0a8f5ec40d52ef3986f75fc529390e20d9015394
NikaIlina <120678649+NikaIlina@users.noreply.github.com>
2022-12-27 15:15:24 +0200

    HW3_2

---
+++ Project/src/Lesson0301/UserPassword.java
@@ -0,0 +1,35 @@
+package Lesson0301;
+
+import java.util.Scanner;
+// Task 2
+public class UserPassword {
+    public static void main(String[] args) {
+
+        String usernamecorrect = "Admin";
+        String passwordcorrect = "P@ssword";
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your User name:");
+
+        String username = input.nextLine();
+
+        if (username.equalsIgnoreCase(usernamecorrect)) {
+            System.out.println("Enter your password:");
+
+            String password = input.nextLine();
+
+            if (password.equals(passwordcorrect)) {
+                System.out.println("Hello " + username + " you have logged in successfully!");
+            }
+            else {
+                System.out.println("Wrong password");
+            }
+        }
+        else {
+            System.out.println("User doesn't exist");
+
+        }
+
+    }
+}
